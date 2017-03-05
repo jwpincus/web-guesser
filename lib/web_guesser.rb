8247e6f4d56b0267a86f58d9d2d8c705d73149a0
Jack Pincus <jwpincus@gmail.com>
2017-03-05 16:13:29 -0700

    base functionality is working

--- lib/web_guesser.rb
+++ lib/web_guesser.rb
@@ -1,6 +1,28 @@
 require "sinatra"
 require 'sinatra/reloader'
+require 'pry-state'
 secret_number = rand(1..100)
+def check_guess(guess, number)
+  if guess != (nil || "")
+    if guess.to_i > number && ((guess.to_i - number).abs < 6)
+      "Too high"
+    elsif guess.to_i < number && ((guess.to_i - number).abs < 6)
+      "Too low"
+    elsif guess.to_i < number
+      "Way Too Low"
+    elsif guess.to_i > number
+      "Way Too High"
+    elsif guess.to_i == number
+      "Nailed it!"
+    end
+  else 
+    "Make a Guess"
+  end
+  
+end
 get "/" do 
-  erb :index, :locals => {:number => secret_number}
+  response = check_guess(@params['guess'], secret_number)
+  correct_answer = "The Number is actually #{secret_number}" if (response == "Nailed it!")
+  erb :index, :locals => {:number => correct_answer, :response => response}
+
 end
