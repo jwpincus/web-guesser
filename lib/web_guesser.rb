2f4c90b321854427062af6f08f8e0cac5f06e882
Jack Pincus <jwpincus@gmail.com>
2017-03-05 17:52:14 -0700

    added color changing background

--- lib/web_guesser.rb
+++ lib/web_guesser.rb
@@ -3,8 +3,9 @@ require 'sinatra/reloader'
 require 'pry-state'
 secret_number = rand(1..100)
 def check_guess(guess, number)
-  if guess != (nil || "")
-    if guess.to_i > number && ((guess.to_i - number).abs < 6)
+    if guess == nil
+      "Make a Guess"
+    elsif guess.to_i > number && ((guess.to_i - number).abs < 6)
       "Too high"
     elsif guess.to_i < number && ((guess.to_i - number).abs < 6)
       "Too low"
@@ -15,14 +16,22 @@ def check_guess(guess, number)
     elsif guess.to_i == number
       "Nailed it!"
     end
-  else 
-    "Make a Guess"
+  
+end
+
+def background(response)
+  case response 
+    when "Nailed it!" then "#00bc3e"
+    when ("Too low") then "#ff7777"
+    when ("Too high") then "#ff7777"
+    when ("Way Too low" || "Way Too high") then "#f90909"
+    else "#f90909"
   end
   
 end
 get "/" do 
   response = check_guess(@params['guess'], secret_number)
   correct_answer = "The Number is actually #{secret_number}" if (response == "Nailed it!")
-  erb :index, :locals => {:number => correct_answer, :response => response}
+  erb :index, :locals => {:number => correct_answer, :response => response, :background => background(response)}
 
 end
